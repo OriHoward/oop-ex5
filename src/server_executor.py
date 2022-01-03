114076531a88b50729cd3ae63083965602a4e919
adiy55 <adiyafe55@gmail.com>
2022-01-03 10:57:22 +0200

    added server_executor.py

---
+++ src/server_executor.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+from subprocess import check_output
+
+stage_id = '0'
+print(os.getcwd())
+os.chdir('../')
+jar_path = os.path.join("Ex4_Server_v0.0.jar")
+subprocess.Popen(['java', '-jar', jar_path, stage_id],stdin=None, stdout=None, stderr=None)
