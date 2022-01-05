c807d58900118a8376119ade79a17dd309261ce1
adiy55 <adiyafe55@gmail.com>
2022-01-05 16:16:21 +0200

    Update ConstansUI.py

--- src/ConstansUI.py
+++ src/ConstansUI.py
@@ -6,7 +6,7 @@ pygame.font.init()
 BACKGROUND_COLOR = Color(30, 30, 30)
 GAME_FONT = font.SysFont("Arial", 20, bold=True)
 SCREEN_CAPTION = "Happy This Is the Last Assignment"
-SCREEN_ICON = r'../misc/icon.png'
+SCREEN_ICON = r'../misc/pokeball.png'
 LINE_COLOR = Color(255, 255, 255)
 CIRCLE_COLOR = Color(128, 216, 255)
 TEXT_COLOR = Color(222, 22, 22)
