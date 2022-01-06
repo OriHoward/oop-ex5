f796bb79c5190b6900cfcaa664dfd779a9774dcf
adiy55 <adiyafe55@gmail.com>
2022-01-06 17:47:26 +0200

    Add documentation to agent.py and Drawable.py

--- src/Drawable.py
+++ src/Drawable.py
@@ -5,12 +5,21 @@ class Drawable(ABC):
 
     @abstractmethod
     def get_icon_path(self):
+        """
+        Image file path.
+        """
         pass
 
     @abstractmethod
     def get_icon_proportions(self):
+        """
+        Size of icon to display.
+        """
         pass
 
     @abstractmethod
     def get_pos(self):
+        """
+        Position of object.
+        """
         pass
--- src/agent.py
+++ src/agent.py
@@ -24,12 +24,21 @@ class Agent(Drawable):
         self._assigned_path = []
 
     def set_assigned_path(self, path: list):
+        """
+        Set current shortest path to the pokemon assigned to the agent.
+        """
         self._assigned_path = path
 
     def get_assigned_path(self):
+        """
+        Return current assigned path to the pokemon.
+        """
         return self._assigned_path
 
     def update_agent(self, value, src, dest, speed, pos):
+        """
+        Update agent attributes (used when parsing json).
+        """
         self._value = value
         self._src = src
         self._dest = dest
@@ -37,9 +46,16 @@ class Agent(Drawable):
         self._pos = Position(*(pos.split(",")))
 
     def calculate_load_factor(self, dist: float) -> float:
+        """
+        Calculate load factor of the agent to a given distance (with regards to its current load factor).
+        time = dist / speed
+        """
         return self._load_factor + (dist / self._speed)
 
     def update_load_factor(self, dist: float) -> None:
+        """
+        Add to the load factor of the agent.
+        """
         self._load_factor += dist / self._speed
 
     def get_pos(self):
@@ -52,28 +68,54 @@ class Agent(Drawable):
         return 40, 40
 
     def set_curr_pokemon(self, pokemon: Pokemon):
+        """
+        Set pokemon agent is assigned to.
+        """
         self._curr_pokemon = pokemon
 
     def get_pokemon(self):
+        """
+        Return pokemon agent is assigned to.
+        """
         return self._curr_pokemon
 
-    def set_placement(self, initial_spot):
+    def set_placement(self, initial_spot: int):
+        """
+        Initial node ID where the agent is placed (the start node).
+        Used for testing.
+        """
         self._placement = initial_spot
 
+    def get_placement(self):
+        """
+        Returns agent node placement.
+        Used for testing.
+        """
+        return self._placement
+
     def get_speed(self):
+        """
+        Return speed of agent.
+        """
         return self._speed
 
     def get_dest(self):
+        """
+        Destination is -1 if the agent reached its destination.
+        """
         return self._dest
 
     def get_src(self):
+        """
+        Return current node ID of where the agent is placed.
+        """
         return self._src
 
     def get_id(self):
+        """
+        Return agent ID number.
+        """
         return self._id
-
-    def get_placement(self):
-        return self._placement
 
     def __repr__(self):
         return f"Agent: {self._id}, {self._value}, {self._pos}, {self._src}"
